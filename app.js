a37e1fbacb28ef3ea5a02d9643d9f8dbcfa0f2ad
Reymago23 <elderriqui@gmail.com>
2022-09-25 16:07:07 -0600

    add routing and initial controllers

--- app.js
+++ app.js
@@ -1,1 +1,35 @@
-console.log('04 Store API')
+require('dotenv').config()
+// async erros
+require('express-async-errors')
+const express = require('express')
+const app = express()
+const notFoundMiddleware = require('./middleware/not-found')
+const errorMiddleware = require('./middleware/error-handler')
+const connectDB = require('./db/connect')
+const productsRouter = require('./routes/products')
+const { startSession } = require('mongoose')
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send('<h1>Store API</h1><a href="/api/v1/products">products route</a>')
+})
+
+app.use('/api/v1/products', productsRouter)
+
+
+app.use(notFoundMiddleware)
+app.use(errorMiddleware)
+
+const port = process.env.PORT || 3000
+
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URI)
+        app.listen(port, console.log(`Server started on port ${port}`))
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+start()
